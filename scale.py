d2052de6013dd3951b7081122d2c3e40db191ff7
Dariel Pagdulagan <darielpagdulagan@yahoo.com>
2025-03-18 00:37:57 +0800

    updated sensors

--- scale.py
+++ scale.py
@@ -2,7 +2,14 @@ import RPi.GPIO as GPIO
 from hx711 import HX711
 
 GPIO.setmode(GPIO.BCM)
-hx = HX711(dout_pin=6, pd_sck_pin=5)
-while True:
-    reading = hx.get_raw_data_mean()
-    print(reading)
+GPIO.setwarnings(False)  
+hx = HX711(5, 6)
+hx.set_reading_format("MSB", "MSB")
+referenceUnit = 20.22
+hx.set_reference_unit(referenceUnit)
+hx.reset()
+hx.tare()
+weight = hx.get_weight(5)
+weight = f"{weight/1000:.2f}"
+
+print(weight)
