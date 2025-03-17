729d718734684d17960ffc9d062ed45d8e61906f
Dariel Pagdulagan <darielpagdulagan@yahoo.com>
2025-03-18 01:11:20 +0800

    included powerdown

--- scale.py
+++ scale.py
@@ -39,7 +39,7 @@ because if I used the 114000, I'd be getting milligrams instead of grams.
 '''
 
 referenceUnit = 10.58
-#hx.set_reference_unit(referenceUnit)
+hx.set_reference_unit(referenceUnit)
 
 hx.reset()
 
