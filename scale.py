ce1991f35fd7b1de88606d2b6a4a041240e90f30
Dariel Pagdulagan <darielpagdulagan@yahoo.com>
2025-03-18 01:13:33 +0800

    included powerdown

--- scale.py
+++ scale.py
@@ -38,8 +38,8 @@ In my case, the longValueWithOffset was around 114000 so my reference unit is 11
 because if I used the 114000, I'd be getting milligrams instead of grams.
 '''
 
-referenceUnit = 10.58
-hx.set_reference_unit(referenceUnit)
+#referenceUnit = 10.58
+#hx.set_reference_unit(referenceUnit)
 
 hx.reset()
 
