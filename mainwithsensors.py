9d0523b29e81ab1bc73a3b5035561a30b95ab7a7
Dariel Pagdulagan <darielpagdulagan@yahoo.com>
2025-03-18 14:08:41 +0800

    included powerdown

--- mainwithsensors.py
+++ mainwithsensors.py
@@ -278,21 +278,24 @@ class BMICalculator(tk.Tk):
             GPIO.output(TRIG, False)
 
             def measure_distance():
-                GPIO.output(TRIG, True)
-                time.sleep(0.00001)
-                GPIO.output(TRIG, False)
+                try:
+                    GPIO.output(TRIG, True)
+                    time.sleep(0.00001)
+                    GPIO.output(TRIG, False)
 
-                while GPIO.input(ECHO) == 0:
-                    pulse_start = time.time()
+                    while GPIO.input(ECHO) == 0:
+                        pulse_start = time.time()
 
-                while GPIO.input(ECHO) == 1:
-                    pulse_end = time.time()
+                    while GPIO.input(ECHO) == 1:
+                        pulse_end = time.time()
 
-                pulse_duration = pulse_end - pulse_start
-                distance = (pulse_duration * SPEED_OF_SOUND) / 2
-                if distance < 2 or distance > 400:  # Assuming sensor range is 2cm to 400cm
-                    raise ValueError("Out of range measurement")
-                return round(distance, 2)
+                    pulse_duration = pulse_end - pulse_start
+                    distance = (pulse_duration * SPEED_OF_SOUND) / 2
+                    if distance < 2 or distance > 400:  # Assuming sensor range is 2cm to 400cm
+                        raise ValueError("Out of range measurement")
+                    return round(distance, 2)
+                except:
+                    print("failed")
     
         except:
             if self.retries!=3:
