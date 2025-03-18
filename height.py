12a3cc003264548d7c578a528d8c816b0b091733
Dariel Pagdulagan <darielpagdulagan@yahoo.com>
2025-03-18 22:20:11 +0800

    added table

--- height.py
+++ height.py
@@ -14,23 +14,26 @@ def gather_height():
     GPIO.output(TRIG, False)
 
     def measure_distance():
-        GPIO.output(TRIG, True)
-        time.sleep(0.00001)
-        GPIO.output(TRIG, False)
+        try:
+            GPIO.output(TRIG, True)
+            time.sleep(0.00001)
+            GPIO.output(TRIG, False)
 
-        while GPIO.input(ECHO) == 0:
-            pulse_start = time.time()
+            while GPIO.input(ECHO) == 0:
+                pulse_start = time.time()
 
-        while GPIO.input(ECHO) == 1:
-            pulse_end = time.time()
+            while GPIO.input(ECHO) == 1:
+                pulse_end = time.time()
 
-        pulse_duration = pulse_end - pulse_start
+            pulse_duration = pulse_end - pulse_start
 
-        distance = (pulse_duration * SPEED_OF_SOUND) / 2
-        return round(distance, 2)
+            distance = (pulse_duration * SPEED_OF_SOUND) / 2
+            return round(distance, 2)
+        except: 
+            pass
 
     print("Waiting for the sensor to settle")
-    time.sleep(1)
+    time.sleep(0.002)
 
     while True:
         results = []
